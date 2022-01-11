6349db9a34f9ed99936d305c3a963ec4d788dab8
PranayChavhan <pranay21chavhan@outlook.com>
2022-01-11 19:55:32 +0530

    Day 1

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+import Header from './Header'
+
+const Home = () => {
+    return (
+        <div>
+            <Container>
+            <Header/>
+            home
+
+            </Container>
+        </div>
+    )
+}
+
+const Container = styled.div`
+    margin-top: 40px;
+`;
+
+export default Home
